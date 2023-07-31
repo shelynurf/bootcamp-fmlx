31f0f7ce87650217a065cdbe4ba7532d0a3f8104
shelynurf <shelynurfitriani@gmail.com>
2023-07-31 21:30:24 +0700

    clean repo

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CatLibrary; // to import namespace
+class Program
+
+{
+	static void Main()
+	
+	{
+		Console.WriteLine("Hello World2");
+		Cat alex = new Cat();
+		alex.name = "alex";
+		alex.age = 2;
+		alex.Eat();
+		string name = alex.name;
+		Console.WriteLine(name);
+		
+		Cat pedro = new Cat();
+		pedro.name = "pedro";
+		pedro.age = 5;
+		pedro.MakeSound();
+		
+	}
+}
