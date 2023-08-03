2b8dadc510de9ff063f31db3fcfce9bdfea36b8a
shelynurf <shelynurfitriani@gmail.com>
2023-08-03 16:52:19 +0700

    feat abstraction and interface project

---
+++ abstract-practice/Program.cs
@@ -0,0 +1,104 @@
+﻿public abstract class Family
+
+{
+	public string name;
+	protected int _age;
+	private int _nik;
+	
+	public Family(string name, int age, int nik)
+	
+	{
+		this.name = name;
+		_age = age;
+		_nik = nik;
+	}
+	public virtual void isMarried()
+	
+	{
+		Console.WriteLine(name + " is married");
+	}
+	
+	public abstract void Role();
+}
+
+public class Mom : Family
+
+{
+	public Mom(string name, int age, int nik) : base(name, age, nik)
+	
+	{
+		
+	}
+	public override void Role()
+	{
+		Console.WriteLine(name + " Managing house");
+	}
+}
+
+public class Dad : Family
+
+{
+	public Dad(string name, int age, int nik) : base(name, age, nik)
+	
+	{
+		
+	}
+	public override void Role()
+	{
+		Console.WriteLine(name + " Looking for money");
+	}
+}
+
+public class Children : Family
+
+{
+	public Children(string name, int age, int nik) : base(name, age, nik)
+	{}
+	public override void Role()
+	{
+		Console.WriteLine(name + " Study at school");
+	}
+
+	public override void isMarried()
+	{
+		Console.WriteLine(name + " is not married");
+	}
+	
+	public void Criteria()
+	
+	{
+		if (_age < 25 )
+		
+		{
+			Console.WriteLine(name + " is Teenager");
+			
+		}
+		else
+		
+		{
+			Console.WriteLine("Adult");
+		}
+	}
+	
+}
+
+class Program
+
+{
+	static void Main()
+	
+	{
+		Mom mom = new Mom("mom", 45, 12345);
+		Dad dad = new Dad("dad", 50, 23456);
+		Children children = new Children("children", 18, 34567);
+		
+		
+		
+		mom.isMarried();
+		children.Criteria();
+		dad.Role();
+		
+		
+		
+	}
+}
---
+++ properties/Program.cs
@@ -0,0 +1,31 @@
+﻿class Car {
+	//Variable
+	public int x;
+	public string y;
+	public IEngine engine;
+	
+	//Property or Variable Method 
+	public int z { get; private set; }
+	public string q { 
+	get
+	{
+		return q + "World";
+	}
+	set
+	{ 
+		q = value + "Hello";
+	} 
+	}
+	
+	//Method
+	public void Run() { }
+	public int Stop() { return 0; }
+	public int Stop(int x) { return x; }
+}
+
+public interface IEngine {
+	public int x {get; set;}
+
+	void Run();
+	void Start();
+}
