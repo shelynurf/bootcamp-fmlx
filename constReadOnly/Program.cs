25c2012ede203bfe6a695ad34b9dcea64c0cd220
shelynurf <shelynurfitriani@gmail.com>
2023-08-07 17:00:13 +0700

    ref

---
+++ constReadOnly/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program {
+	static void Main() {
+		Calculator calc = new(6);
+		Console.WriteLine(Calculator.phi);
+        Console.WriteLine(calc.x);
+	}
+}
+class Calculator{
+	public const float phi = 3.14f; //static & must assign the value
+	public readonly int x; //can empty, modified it from Constructor
+	public Calculator(int value) {
+		x = value;
+	}	
+}
---
+++ string-stringBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+public class Program
+{
+	static void Main()
+	{
+		int iterations = 10000;
+		string resultString = "";
+		StringBuilder resultStringBuilder = new StringBuilder();
+		
+		Stopwatch stopwatch1 = Stopwatch.StartNew();
+		for (int i = 0; i < iterations; i++)
+		{
+			resultString += "a";
+		}
+		stopwatch1.Stop();
+		
+		Console.WriteLine($"Time String : {stopwatch1.ElapsedMilliseconds} ms");
+
+		Stopwatch stopwatch2 = Stopwatch.StartNew();
+		for (int i = 0; i < iterations; i++)
+		{
+			resultStringBuilder.Append("a");
+		}
+		stopwatch2.Stop();
+		
+		Console.WriteLine($"Time SB: {stopwatch2.ElapsedMilliseconds} ms");
+		Console.ReadLine();
+	}
+}
