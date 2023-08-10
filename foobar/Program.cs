b1fab2aa398ff234e66206fcf9eb6d5945464270
shelynurf <shelynurfitriani@gmail.com>
2023-08-10 17:01:07 +0700

    operator overloading

---
+++ foobar/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+
+{
+	static void Foobar(int x)
+	
+	{
+		for(int i = 0; i <= x; i++)
+		
+		{
+			
+			if (i > 0 && i % 3 == 0)
+			
+			{
+				if (i % 3 == 0 && i % 5 == 0)
+				
+				{
+					Console.WriteLine("foobar");
+				}
+				else 
+				
+				{
+					Console.WriteLine("foo");
+				}
+			}
+			else if (i > 0 && i % 5 == 0)	
+			
+			{
+				Console.WriteLine("bar");
+			}
+			else
+			
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+	static void Main()
+	
+	{	Console.Write("Input your number : ");
+		int input = Convert.ToInt32(Console.ReadLine());
+		Foobar(input);
+		
+	}
+}
---
+++ operatorOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Family
+{
+	public string name;
+	
+	public Family(string name)
+	{
+		this.name = name;
+	}
+	
+	public static Family operator + (Family family1, Family family2)
+	{
+		return new Family(family1.name + family2.name);
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Family family1 = new Family("Agus");
+		Family family2 = new Family("Bella");
+		Family family3 = family1 + family2;
+		Console.WriteLine(family3.name);
+	}
+}
