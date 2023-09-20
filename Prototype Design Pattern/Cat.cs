215aa80f99b86c434b7db915e491f5b7ac401c0c
shelynurf <shelynurfitriani@gmail.com>
2023-09-20 12:14:01 +0700

    add content prototype

---
+++ Prototype Design Pattern/Cat.cs
@@ -0,0 +1,7 @@
+public class Cat
+{
+    public string catName;
+    public int age;
+    public Fur fur;
+    public string ownerName;
+}
---
+++ Prototype Design Pattern/Fur.cs
@@ -0,0 +1,6 @@
+public class Fur
+{
+    public string furColor;
+    public string furPattern;
+    
+}
